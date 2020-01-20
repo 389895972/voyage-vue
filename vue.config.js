be62ff982c48487dca55687e322eb05c4edc5555
389895972 <49613894+389895972@users.noreply.github.com>
2020-01-20 20:27:11 +0800

    解决114

--- vue.config.js
+++ vue.config.js
@@ -12,14 +12,14 @@ module.exports = {
                     '^/api': ''  //通过pathRewrite重写地址，将前缀/api转为/
                 }
             },
-            '/api/v2':{
-                target:'http://10.0.20.114:10088',
-                ws: true,
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api/v2': ''  //通过pathRewrite重写地址，将前缀/api转为/
-                }
-            }
+            // '/api/v2':{
+            //     target:'http://10.0.20.114:10088',
+            //     ws: true,
+            //     changeOrigin: true,
+            //     pathRewrite: {
+            //         '^/api/v2': ''  //通过pathRewrite重写地址，将前缀/api转为/
+            //     }
+            // }
         }
 
 }}
